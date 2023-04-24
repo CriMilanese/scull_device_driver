fc6a98d10b090300d8224b0ab27c4ed99b8b02ba
Cristiano <c.milanese@student.vu.nl>
2023-04-24 05:04:40 -0400

    rw operations

--- rust/my_null.rs
+++ rust/my_null.rs
@@ -1,40 +1,79 @@
-//! loads a custom implementation of the null device driver as a module
+// SPDX-License-Identifier: GPL-2.0
+
+//! Rust miscellaneous device sample.
 
 use kernel::prelude::*;
-use kernel::{file, chrdev};
+use kernel::{
+    file::{File, Operations},
+    io_buffer::{IoBufferReader, IoBufferWriter},
+    miscdev,
+    sync::{Arc, ArcBorrow},
+};
 
 module! {
-  type: IsChrDev,
-  name: "isnull",
-  license: "GPL",
+    type: NullDeviceModule,
+    name: "isnull",
+    author: "Cristiano Milanese",
+    description: "Rust null device sample",
+    license: "GPL",
 }
 
-struct IsNull;
+struct NullDevice {
+  number: usize,
+  contents: Vec<u8>,
+}
 
 #[vtable]
-impl file::Operations for IsNull {
-  fn open(_shared: &(), _file: &file::File) -> Result<Self::Data> {
-    pr_info!("opening this device file\n");
-    Ok(())
-  }
+impl Operations for NullDevice {
+
+    type OpenData = Arc<NullDevice>;
+    type Data = Arc<NullDevice>;
+
+    fn open(data: &Self::OpenData, _file: &File) -> Result<Self::Data> {
+        pr_info!("opened device with minor number {}\n", data.number);
+        Ok(data.clone())
+    }
+
+    fn read(
+        _this: ArcBorrow<'_, NullDevice>,
+        _: &File,
+        _data: &mut impl IoBufferWriter,
+        _offset: u64,
+    ) -> Result<usize> {
+        Ok(0)
+    }
+
+    fn write(
+        _this: ArcBorrow<'_, NullDevice>,
+        _: &File,
+        data: &mut impl IoBufferReader,
+        _offset: u64,
+    ) -> Result<usize> {
+        Ok(data.len())
+    }
 }
 
-struct IsChrDev {
-  // Pin a pointer to a Box (aka allocated memory) as big as a Registration for char device
-  _dev: Pin<Box<chrdev::Registration<1>>>,
+struct NullDeviceModule {
+    _dev: Pin<Box<miscdev::Registration<NullDevice>>>,
 }
 
-impl kernel::Module for IsChrDev {
-  fn init(_name: &'static CStr, _module: &'static ThisModule) -> Result<Self> {
-    pr_info!("loading IsChrDev module\n");
-    // question mark at the end allows us to dodge error handling
-    let reg = chrdev::Registration::new_pinned(_name, 0, _module)?;
-    Ok(IsChrDev { _dev: reg })
-  }
+impl kernel::Module for NullDeviceModule {
+    fn init(name: &'static CStr, _module: &'static ThisModule) -> Result<Self> {
+
+        let dev = Arc::try_new(NullDevice {
+		number: 0,
+		contents: Vec::new(),
+	})?;
+        pr_info!("loading null device driver\n");
+        let reg = miscdev::Registration::<NullDevice>::new_pinned(fmt!("{name}"), dev)?;
+        Ok(NullDeviceModule {
+            _dev: reg,
+        })
+    }
 }
 
-impl Drop for IsChrDev {
+impl Drop for NullDeviceModule {
     fn drop(&mut self) {
-        pr_info!("Unloading IsChrDev module\n");
+        pr_info!("Unloading null device driver\n");
     }
 }
