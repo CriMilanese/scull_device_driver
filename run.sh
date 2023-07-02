b51f069dd45a30a74275b0326dcb6dadb24feeef
CriMilanese <milanese.cristiano2@gmail.com>
2023-07-02 22:46:04 +0200

    skip main branch

--- run.sh
+++ run.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
 
 GRAPH_NAME="rw_latency_"
-BRANCH=$(git branch --show-current)
+FIO_OUT_DIR="results/"
+BRANCHES=$(git for-each-ref --format='%(refname:short)' refs/heads/)
+
+GREEN='\033[0;34m'
+RED='\033[0;31m'
+NC='\033[0m'
 
 if [[ $EUID != 0 ]];
 then
-    echo "Please run as root"
+    echo "${RED}Please run as root${NC}"
     exit
 fi
 
+for branch in $BRANCHES;
+do
+  if[[$branch == "main "]]; then
+    # skip devel branch
+  	continue;
+  done
 
-pushd rust
-make; make unload &>/dev/null; make load
-popd
+  git checkout $branch
 
-echo -e "rust scull module loaded\n"
+	pushd rust
+	make; make unload &>/dev/null; make load
+	popd
 
-pushd c
-make; make unload &>/dev/null; make load
-popd
+	echo -e "${GREEN}rust scull module loaded\n${NC}"
 
-echo -e "c scull module loaded\n"
+	pushd c
+	make; make unload &>/dev/null; make load
+	popd
 
-echo $GRAPH_NAME$BRANCH
-fio jf.fio --output-format=json --output=$GRAPH_NAME$BRANCH
-python3 fio-parser-plotter.py $GRAPH_NAME$BRANCH
+	echo -e "${GREEN}c scull module loaded\n${NC}"
+
+	fio jf.fio --output-format=json --output=$FIO_OUT_DIR$GRAPH_NAME$branch --minimal
+	echo -e "${GREEN}tested ${NC}" $GRAPH_NAME$branch
+	python3 fio-parser-plotter.py $GRAPH_NAME$branch
+ 	echo -e "${GREEN}plotted${NC}"
+done
+
